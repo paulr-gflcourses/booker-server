97be9537abdba5ddf81f46286abc1629c55c1ab3
paulr-gflcourses <paulgerald123@gmail.com>
2018-09-26 16:27:17 +0300

    dev on win

--- server/libs/RestServer.php
+++ server/libs/RestServer.php
@@ -20,10 +20,10 @@ class RestServer
     {
         $this->service = $service;
         $url = $_SERVER['REQUEST_URI'];
-        //list($b, $c, $s, $a, $d, $e, $db, $table, $path) = explode('/', $url, 9);
-        //$params = explode('/', $url, 9);
-         list( $c, $s, $a, $d, $db, $table, $path) = explode('/', $url, 7);
-         $params = explode('/', $url, 7);
+        list($b, $c, $s, $a, $d, $e, $db, $table, $path) = explode('/', $url, 9);
+        $params = explode('/', $url, 9);
+        //  list( $c, $s, $a, $d, $db, $table, $path) = explode('/', $url, 7);
+        //  $params = explode('/', $url, 7);
 
         $method = $_SERVER['REQUEST_METHOD'];
         $funcName = ucfirst($table);
@@ -35,18 +35,18 @@ class RestServer
 
         $result = '';
         $viewType = '.json';
-        // header('Access-Control-Allow-Origin: *');
-        // header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-        // header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
         // header('Access-Control-Allow-Origin: *');
         // header('Access-Control-Allow-Methods: POST,GET,PUT,DELETE');
         // header('Access-Control-Allow-Headers: Authorization, Lang');
 
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-        header('Access-Control-Max-Age: 1000');
-        header('Access-Control-Allow-Headers: *');
+        // header('Access-Control-Allow-Origin: *');
+        // header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        // header('Access-Control-Max-Age: 1000');
+        // header('Access-Control-Allow-Headers: *');
         
         switch ($method) {
         case 'GET':
